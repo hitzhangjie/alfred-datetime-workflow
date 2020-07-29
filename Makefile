5d55dda8cd0c7fae50fd83d900cb77463a8b6051
hitzhangjie <hit.zhangjie@gmail.com>
2020-07-29 22:15:57 +0800

    一个简单的可运行的workflow

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
+timestamp := $(shell date +%s)
 
 test:
 	go build
-	alfred_workflow_bundleid=hit.zhangjie.app alfred_workflow_cache=tmp alfred_workflow_data=tmp ./alfred-datetime-workflow $(date +%s)
+	alfred_workflow_bundleid=hit.zhangjie.app alfred_workflow_cache=tmp alfred_workflow_data=tmp ./alfred-datetime-workflow ${timestamp}
 	rm -rf tmp
--- main.go
+++ main.go
@@ -1,16 +1,54 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io/ioutil"
+	"path/filepath"
+	"time"
 
 	aw "github.com/deanishe/awgo"
 )
 
+var (
+	workflow *aw.Workflow
+
+	//icon = &aw.Icon{
+	//	Value: "com.apple.safari.bookmark",
+	//	Type:  aw.IconTypeFileType,
+	//}
+)
+
+const (
+	helpURL = "https://github.com/hitzhangjie/alfred-datetime-workflow"
+)
+
 func main() {
-	workflow := aw.New()
+	workflow = aw.New(aw.HelpURL(helpURL))
 	workflow.Run(run)
 }
 
 func run() {
-	fmt.Println("hello world")
+
+	// 获取参数
+	args := workflow.Args()
+	// don't log to stdout
+	//fmt.Println(args)
+
+	workflow.NewItem("this is a result").Valid(true).
+		Arg(args[0]).
+		Title("this is title").
+		Subtitle("this is subtitle")
+	workflow.SendFeedback()
+
+	buf := &bytes.Buffer{}
+
+	fp := filepath.Join("/Users/zhangjie/Github/alfred-datetime-workflow/test.log")
+	dat, err := ioutil.ReadFile(fp)
+	if err == nil && len(dat) != 0 {
+		buf = bytes.NewBuffer(dat)
+	}
+
+	buf.WriteString(fmt.Sprintf("time: %v, args: %v\n", time.Now(), args))
+	ioutil.WriteFile(fp, buf.Bytes(), 0666)
 }
